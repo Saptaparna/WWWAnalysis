b8f3bc91b96370c55a34282a17db72446cc7ac91
Saptaparna Bhattacharya <saptaparna@gmail.com>
2019-06-30 19:58:14 -0500

    Python code to read LHEs

---
+++ FourLeptons/ZZZTo4l2nu/readZZZ4l2nuLHE.py
@@ -0,0 +1,60 @@
+import sys
+import ROOT as rt
+import math
+from LHEevent import *
+from LHEfile import *
+import plotTools
+
+if __name__ == '__main__':
+
+    #Bprime histograms
+    h_Lepton_1_Pt = rt.TH1F("h_Lepton_1_Pt", "h_Lepton_1_Pt", 1000, 0., 1000)
+    h_Lepton_1_Pt.Sumw2()
+    h_Lepton_2_Pt = rt.TH1F("h_Lepton_2_Pt", "h_Lepton_2_Pt", 1000, 0., 1000)
+    h_Lepton_2_Pt.Sumw2()
+    h_Lepton_3_Pt = rt.TH1F("h_Lepton_3_Pt", "h_Lepton_3_Pt", 1000, 0., 1000)
+    h_Lepton_3_Pt.Sumw2()
+    h_Lepton_4_Pt = rt.TH1F("h_Lepton_4_Pt", "h_Lepton_4_Pt", 1000, 0., 1000)
+    h_Lepton_4_Pt.Sumw2()
+    h_MET = rt.TH1F("h_MET", "h_MET", 1000, 0., 1000)
+    h_MET.Sumw2()
+    nu1_lv = rt.TLorentzVector()
+    nu2_lv = rt.TLorentzVector()
+
+    # find events in file
+    myLHEfile = LHEfile(sys.argv[1])
+    myLHEfile.setMax(100000)
+    #myLHEfile.setMax(1)
+    eventsReadIn = myLHEfile.readEvents()
+    for oneEvent in eventsReadIn:
+        myLHEevent = LHEevent()
+        myLHEevent.fillEvent(oneEvent)
+        n_l = 0
+        nu = 0
+        transverseMomentum = []
+        for i in range(0,len(myLHEevent.Particles)):
+            p = myLHEevent.Particles[i]
+            if (abs(p['ID']) == 11 or abs(p['ID']) == 13 or abs(p['ID']) == 15): 
+              transverseMomentum.append(rt.TMath.Sqrt(p['Px']*p['Px'] + p['Py']*p['Py']))
+              transverseMomentum.sort(reverse=True)
+              #print transverseMomentum
+              n_l += 1
+              if n_l==4:  print n_l
+              if n_l==4:  h_Lepton_1_Pt.Fill(transverseMomentum[0])
+              if n_l==4:  h_Lepton_2_Pt.Fill(transverseMomentum[1])
+              if n_l==4:  h_Lepton_3_Pt.Fill(transverseMomentum[2])
+              if n_l==4:  h_Lepton_4_Pt.Fill(transverseMomentum[3])
+            if (p['ID'] == 12 or p['ID'] == 14 or p['ID'] == 16): nu1_lv = rt.TLorentzVector(p['Px'], p['Py'], p['Pz'], p['E'])
+            if (p['ID'] == -12 or p['ID'] == -14 or p['ID'] == -16): nu2_lv = rt.TLorentzVector(p['Px'], p['Py'], p['Pz'], p['E'])
+            if (abs(p['ID']) == 12 or abs(p['ID']) == 14 or abs(p['ID']) == 16): nu += 1
+            if (nu==2): h_MET.Fill((nu1_lv+nu2_lv).Pt());
+        del oneEvent, myLHEevent
+        
+    # write the histograms
+    histoFILE = rt.TFile(sys.argv[2],"RECREATE")
+    h_Lepton_1_Pt.Write()
+    h_Lepton_2_Pt.Write()
+    h_Lepton_3_Pt.Write()
+    h_Lepton_4_Pt.Write()
+    h_MET.Write()
+    histoFILE.Close()
